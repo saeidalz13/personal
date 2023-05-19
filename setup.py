c5e43f1c9c0d5163dab35ad1d07b85ed52b8cb91
Saeid Alizadeh <saeid.alizadeh@rwdi.com>
2023-05-19 16:47:02 -0600

    Bugs fixed

--- setup.py
+++ setup.py
@@ -5,11 +5,7 @@ setup(
     name="saeid",
     version="1.0",
     packages=find_packages(),
-    package_data={
-        "your-package": [
-            str(Path("database", "TD_Hours.xlsx"))
-        ]  # Replace 'data/*.xlsx' with the pattern matching your Excel files
-    },
+    package_data={"saeid": [str(Path("database", "TD_Hours.xlsx"))]},
     entry_points={
         "console_scripts": ["saeid=saeid.wrapper_saeid.wrapper:main"],
     },
