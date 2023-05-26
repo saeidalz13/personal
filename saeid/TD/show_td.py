56e93a4365f6b4b8d1ac7dce359b4b38b50c6b03
Saeid Alizadeh <saeid.alizadeh@rwdi.com>
2023-05-25 22:33:02 -0600

    show_td updated

--- saeid/TD/show_td.py
+++ saeid/TD/show_td.py
@@ -5,21 +5,28 @@ from datetime import datetime
 from pathlib import *
 
 path_database = Path(__file__).parent
-DATABASE = Path(path_database, 'td_hours.db')
+DATABASE = Path(path_database, "td_hours.db")
 
 
-def main(database=DATABASE):
+def show_td(database=DATABASE):
     try:
         connect = sqlite3.connect(database)
 
         c = connect.cursor()
-    
+
         with connect:
-            c.execute("SELECT SUM(hours) FROM td_hours")
-            print(c.fetchone()[0])
-        return True
+            result = c.execute(
+                "SELECT SUM(hours) AS total_hours, MIN(date) AS beg_date FROM td_hours"
+            ).fetchone()
+
+        return result
     except Exception as e:
         return False
+
+
+def main():
+    RESULT = show_td()
+    print(f"Since {RESULT[1]}, {RESULT[0]} hours have been charged.")
 
 
 if __name__ == "__main__":
